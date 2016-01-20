81fd4beac433f9d29ceec0501f61188478122983
Serhii Kashuba <kashubasv@gmail.com>
2016-01-20 23:26:33 +0100

    Add angular

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('KnotsApp', []);
+
+app.controller('MainCtr', function($scope) {
+    $scope.desc = 'Knots of wisdom';
+
+});
--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
 	r.Static("/assets", "./assets")
 
 	r.GET("/", index)
@@ -16,5 +14,5 @@ func main() {
 }
 
 func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{})
+	c.File("./templates/index.html")
 }
